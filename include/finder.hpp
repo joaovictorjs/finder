2c447809e4ad9e8b569babf55c4262382efef9f1
joaovictorjs <skai7ami@gmail.com>
2023-10-24 09:06:26 -0300

    variable and method members

--- include/finder.hpp
+++ include/finder.hpp
@@ -10,7 +10,14 @@ namespace Finder {
 
     class Filter {
         private:
+            std::string m_root_dir;
+            str_vec m_whitelist, m_blacklist;
+
         protected:
+
         public:
+            Filter& set_root_dir(const std::string& a_dir);
+            Filter& set_whitelist(const str_vec& a_whitelist);
+            Filter& set_blacklist(const str_vec& a_blacklist);
     };
 }
