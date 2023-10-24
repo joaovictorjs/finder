523dc6b9b02df32aa50c72002eba98b9552ec1cf
joaovictorjs <skai7ami@gmail.com>
2023-10-24 09:02:51 -0300

    aliases

--- include/finder.hpp
+++ include/finder.hpp
@@ -1,6 +1,13 @@
 #pragma once 
 
+#include <filesystem>
+#include <string>
+#include <vector>
+
 namespace Finder {
+    namespace fs = std::filesystem;
+    using str_vec = std::vector<std::string>;
+
     class Filter {
         private:
         protected:
