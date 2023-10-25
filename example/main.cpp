cdbb23742a25f7a145f5796c3d0d9aab447609bd
joaovictorjs <skai7ami@gmail.com>
2023-10-25 20:25:54 -0300

    example

---
+++ example/main.cpp
@@ -0,0 +1,30 @@
+#include <system_error>
+#include <iostream>
+#include <vector>
+#include  <string>
+
+#include <finder.hpp>
+
+bool on_error_callback(const std::string& a_filepath, const std::system_error& a_error){
+    //std::cout<<"[ ERROR ] "<<a_filepath<<' '<<a_error.what()<<'\n';
+    return true;
+}
+
+int main() {
+    Finder::Filter filter;
+    std::vector<std::string> files_found;
+
+    filter
+        .set_root_dir("/")
+        .set_whitelist({"/home/*.xls", "/home/*.xlsx", "*.txt"})
+        .set_blacklist({"*go*"})
+        .search_recursive(files_found, on_error_callback);
+
+    for(const auto& f : files_found){
+        std::cout<<f<<'\n';
+    }
+
+    std::cout<<files_found.size()<<std::endl;
+
+    return 0;
+}
