ef56d8ee57acd0f10ff1ef8138b202e75a711361
joaovictorjs <skai7ami@gmail.com>
2023-10-25 20:43:11 -0300

    example updated

--- example/main.cpp
+++ example/main.cpp
@@ -6,7 +6,7 @@
 #include <finder.hpp>
 
 bool on_error_callback(const std::string& a_filepath, const std::system_error& a_error){
-    //std::cout<<"[ ERROR ] "<<a_filepath<<' '<<a_error.what()<<'\n';
+    std::cout<<"[ ERROR ] "<<a_filepath<<' '<<a_error.what()<<'\n';
     return true;
 }
 
@@ -15,9 +15,8 @@ int main() {
     std::vector<std::string> files_found;
 
     filter
-        .set_root_dir("/")
-        .set_whitelist({"/home/*.xls", "/home/*.xlsx", "*.txt"})
-        .set_blacklist({"*go*"})
+        .set_root_dir("/home")
+        .set_whitelist({"*.conf"})
         .search_recursive(files_found, on_error_callback);
 
     for(const auto& f : files_found){
