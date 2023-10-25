66142af11ddf7a3b962810feac669a340e3675a2
joaovictorjs <skai7ami@gmail.com>
2023-10-25 20:25:09 -0300

    test search and search_recursive methods

--- test/filter_test.cpp
+++ test/filter_test.cpp
@@ -1,9 +1,13 @@
+#include <fstream>
+#include <iostream>
+
 #include <gtest/gtest.h>
 
 #include "finder.hpp"
 
 namespace fs = std::filesystem;
 using str_vec = std::vector<std::string>;
+using error_callback = std::function<bool(const std::string&, const std::system_error&)>;
 
 class FilterHelper : public Finder::Filter {
     public:
@@ -32,6 +36,44 @@ class FilterHelper : public Finder::Filter {
 
         bool filter(const std::string& a_path){
             return Finder::Filter::filter(a_path);
+        }
+};
+
+class SearchTest : public testing::Test {
+    public:
+        void SetUp() override {
+            str_vec folders {"Downloads", "Documents", "Pictures", "Images", "Videos"};
+            str_vec files {
+                "anime.mp4", 
+                    "anime.mp3", 
+                    "malware.exe", 
+                    "sheet.xls",
+                    "sheet.xlsx", 
+                    "document.doc",
+                    "document.docx", 
+                    "document.pdf",
+                    "nf.xml", 
+                    "archlinux.iso"
+            };
+
+            fs::path root {"home"};
+            fs::create_directory(root);
+
+            for(const auto& folder : folders){
+                fs::path dir {root / folder};
+                fs::create_directory(dir);
+
+                for(const auto& file : files){
+                    fs::path filename {dir / file};
+                    std::ofstream ofs {filename};
+                    ofs << "dummy";
+                    ofs.close();
+                }
+            }
+        }
+
+        void TearDown() override {
+            fs::remove_all("home");
         }
 };
 
@@ -87,6 +129,56 @@ TEST(FilterHelper, filter){
     ASSERT_FALSE(fh.filter("/home/Documents/malware.exe"));
     ASSERT_FALSE(fh.filter("/home/Downloads/windows.mp3"));
     ASSERT_FALSE(fh.filter("/home/Documents/windows.mp4"));
+}
+
+TEST_F(SearchTest, search){
+    Finder::Filter filter;
+    str_vec found {};
+    int err_counter {0};
+    filter.set_root_dir("./home/Downloads");
+
+    error_callback err_cb =[&err_counter](const std::string& a_entry, const std::system_error& a_error){
+        err_counter++;
+        return true;
+    }; 
+
+    filter.search(found, err_cb);
+    ASSERT_EQ(found.size(), 10);
+    found.clear();
+
+    filter.set_whitelist({"*.mp?", "*doc*.*", "*.exe"}).set_blacklist({"*malware*"});
+    filter.search(found, err_cb);
+    ASSERT_EQ(found.size(), 5);
+    found.clear();
+
+    filter.set_root_dir("/not_found");
+    filter.search(found, err_cb);
+    ASSERT_EQ(err_counter, 1);
+}
+
+TEST_F(SearchTest, search_recursive){
+    Finder::Filter filter;
+    str_vec found {};
+    int err_counter {0};
+    filter.set_root_dir("./home");
+
+    error_callback err_cb =[&err_counter](const std::string& a_entry, const std::system_error& a_error){
+        err_counter++;
+        return true;
+    }; 
+
+    filter.search_recursive(found, err_cb);
+    ASSERT_EQ(found.size(), 50);
+    found.clear();
+
+    filter.set_whitelist({"*/Do*/*.mp?", "*/Do*/*doc*.*", "*/Do*/*.exe"}).set_blacklist({"*malware*"});
+    filter.search_recursive(found, err_cb);
+    ASSERT_EQ(found.size(), 10);
+    found.clear();
+
+    filter.set_root_dir("/not_found");
+    filter.search_recursive(found, err_cb);
+    ASSERT_EQ(err_counter, 1);
 }
 
 int main(int argc, char** argv) {
